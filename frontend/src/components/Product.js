54e43ed7774ed1d197ab2cc5cc50877c69e816b0
s-krishnaraju <senvk25@gmail.com>
2023-12-21 22:33:02 -0500

    Added cards for products

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,15 +1,29 @@
-import React from 'react'
-import { Card } from 'react-bootstrap'
+import React from "react";
+import { Card } from "react-bootstrap";
 
-function Product({product}) {
+function Product({ product }) {
   return (
-    <Card className='my-3 p-3 rounded'>
+    <Card className="my-3 p-3 rounded">
       <a href={`/product/${product._id}`}>
-       {product.name}
-       <img src = "images\airpods.jpg"></img>
+        <Card.Img src={product.image}></Card.Img>
       </a>
+      <Card.Body>
+        <a href={`/product/${product._id}`}>
+          <Card.Title>
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </a>
+        <Card.Text>
+          <div>
+            {product.rating}/10 from {product.numReviews} Reviews
+          </div>
+        </Card.Text>
+        <Card.Text as="h3">
+          ${product.price}
+        </Card.Text>
+      </Card.Body>
     </Card>
-  )
+  );
 }
 
-export default Product
+export default Product;
