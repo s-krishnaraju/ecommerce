1403cb2be21dd8de30f7342e4a4fec6336f96d13
s-krishnaraju <senvk25@gmail.com>
2023-12-29 09:22:17 -0500

    Implemented backend auth with JWT

--- backend/backend/base/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("api/", views.getRoutes, name="routes"),
-    path("api/products/", views.getProducts, name="products"),
-    path("api/products/<str:pk>/", views.getProduct, name="product"),
-]
